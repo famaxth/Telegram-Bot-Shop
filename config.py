5108af70a9ead5c44250dacb0eae6b6d45e8cc49
Berlin <70908119+Berl4n@users.noreply.github.com>
2021-05-21 23:21:11 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+token = "token"
+admin_id = 77777777
+
+file_1 = open("edit_qiwi_money_number.txt", "r")
+qiwi_number = file_1.read()
+
+file_2 = open("edit_qiwi_money_token.txt", "r")
+token_qiwi = file_2.read()

582f9616c9cea7412d7ab5a4d15b3f06781c2803
Arseniy Titov <70908119+famaxth@users.noreply.github.com>
2021-08-02 22:24:52 +0300

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,8 @@
+token = "text"
+admin_id = 777777777
+
+file_1 = open("edit_qiwi_money_number.txt", "r")
+qiwi_number = file_1.read()
+
+file_2 = open("edit_qiwi_money_token.txt", "r")
+token_qiwi = file_2.read()
